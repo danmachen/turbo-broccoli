d332e2736da15f86591cfeac6ddee8911eb6c74f
cadtrix <d.p.machen@gmail.com>
2018-01-17 19:57:28 +0000

    Update sma.py

--- sma.py
+++ sma.py
@@ -1,10 +1,9 @@
-#import ta-lib - technical analysis library. 
+#import ta-lib - technical analysis library.
 #https://github.com/mrjbq7/ta-lib/blob/master/README.md
 
 
 import talib as ta
 import numpy as np
-import panda as pd
 
 #import [binance data] into pd.Dataframe
 
@@ -15,3 +14,9 @@ print talib.get_functions()
 
 # dict of functions by group
 print talib.get_function_groups()
+
+# generate random price numbers
+close = numpy.random.random(100)
+
+# calculate simple moving average of close
+output = talib.SMA(close)
